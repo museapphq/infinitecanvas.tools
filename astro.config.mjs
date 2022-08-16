7ddbea9510372ae481136b311bf0b0c4f9e44698
Arun Venkatesan <dallasarun@gmail.com>
2022-08-16 14:22:02 -0700

    added @astrojs/image
    `npm run astro add image`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), image()]
 });
