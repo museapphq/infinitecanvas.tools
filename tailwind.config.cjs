28ca454d5a50c01f3a88757cecd3e6e432f95a51
Arun Venkatesan <dallasarun@gmail.com>
2022-08-24 11:24:56 -0700

    add font stacks to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,56 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      sans: [
+        "IBM Plex Sans Condensed",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        "avenir next",
+        "avenir",
+        "segoe ui",
+        "helvetica neue",
+        "helvetica",
+        "Cantarell",
+        "Ubuntu",
+        "roboto",
+        "noto",
+        "arial",
+        "sans-serif"
+      ],
+      serif: [
+        "Merriweather",
+        "Iowan Old Style",
+        "Apple Garamond",
+        "Baskerville",
+        "Times New Roman",
+        "Droid Serif",
+        "Times",
+        "Source Serif Pro",
+        "serif",
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol"
+      ],
+      title: [
+        "IBM Plex Sans",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        "avenir next",
+        "avenir",
+        "segoe ui",
+        "helvetica neue",
+        "helvetica",
+        "Cantarell",
+        "Ubuntu",
+        "roboto",
+        "noto",
+        "arial",
+        "sans-serif"
+      ]
+    },
+    extend: {}
+  },
+  plugins: []
 }
