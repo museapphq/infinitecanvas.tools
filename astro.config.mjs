4378a83b93c197e1b8594f025693786c66554285
Arun Venkatesan <dallasarun@gmail.com>
2022-08-29 09:58:07 -0700

    randomly select items for gallery

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from "astro/config"
 import tailwind from "@astrojs/tailwind"
-
+import react from "@astrojs/react"
 import image from "@astrojs/image"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
+    image(),
+    react(),
     tailwind({
       config: { applyBaseStyles: false }
-    }),
-    image()
+    })
   ]
 })
---
+++ src/components/IndexRandomGallery.jsx
@@ -0,0 +1,19 @@
+const NUMBER_OF_ITEMS_TO_SELECT = 4
+
+const IndexRandomGallery = ({ galleryItems }) => {
+  const shuffled = galleryItems.sort(() => 0.5 - Math.random())
+  const selected = shuffled.slice(0, NUMBER_OF_ITEMS_TO_SELECT)
+
+  return (
+    <ul class="grid grid-cols-4 gap-24 p-32">
+      {selected.map(({ frontmatter: { title, description } }) => (
+        <li class="p-24 bg-brown bg-opacity-5 rounded-4">
+          <h2 class="mb-4 sans-20 text-gray">{title}</h2>
+          <p class="serif-16 text-brown text-opacity-80">{description}</p>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default IndexRandomGallery
