7c5799fbcecfb008694d236aeaaa2465d152f76d
Arun Venkatesan <dallasarun@gmail.com>
2022-08-29 10:28:30 -0700

    refactor and header image

--- src/components/index/IndexRandomGallery.jsx
+++ src/components/index/IndexRandomGallery.jsx
@@ -5,7 +5,7 @@ const IndexRandomGallery = ({ galleryItems }) => {
   const selected = shuffled.slice(0, NUMBER_OF_ITEMS_TO_SELECT)
 
   return (
-    <ul class="grid grid-cols-4 gap-24 p-32">
+    <ul class="grid grid-cols-4 gap-24 px-32">
       {selected.map(({ frontmatter: { title, description } }) => (
         <li class="p-24 bg-brown bg-opacity-5 rounded-4">
           <h2 class="mb-4 sans-20 text-gray">{title}</h2>
