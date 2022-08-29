fadde3434ded61a4be031a85750370bf394025ad
Arun Venkatesan <dallasarun@gmail.com>
2022-08-29 14:33:39 -0700

    add arun and muse images to footer

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -32,6 +32,9 @@ module.exports = {
       brown: "#49461F",
       transparent: "transparent"
     },
+    dropShadow: {
+      1: "drop-shadow(0px 1px 2px rgba(0, 0, 0, 0.1));"
+    },
     fontFamily: {
       sans: [
         "IBM Plex Sans Condensed",
