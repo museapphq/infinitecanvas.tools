e43865c22f9b96b6497c5bbdfe8795ef10318bfc
Arun Venkatesan <dallasarun@gmail.com>
2022-08-30 13:55:24 -0700

    colocate all images

--- src/components/index/IndexRandomGallery.jsx
+++ src/components/index/IndexRandomGallery.jsx
@@ -5,11 +5,10 @@ const IndexRandomGallery = ({ galleryItems }) => {
   const selected = shuffled.slice(0, NUMBER_OF_ITEMS_TO_SELECT)
 
   return (
-    <ul class="grid grid-cols-4 gap-24 px-32">
-      {selected.map(({ frontmatter: { title, description } }) => (
+    <ul class="grid grid-cols-4 gap-24 px-32 w-full">
+      {selected.map(({ frontmatter: { title } }) => (
         <li class="p-24 bg-brown bg-opacity-5 rounded-4">
           <h2 class="mb-4 sans-20 text-gray">{title}</h2>
-          <p class="serif-16 text-brown text-opacity-80">{description}</p>
         </li>
       ))}
     </ul>
