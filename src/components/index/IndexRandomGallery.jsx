d4b5cdc84839a0b05da0a787893bc5b49fc554cc
Arun Venkatesan <dallasarun@gmail.com>
2022-09-02 11:09:04 -0700

    add placeholder image

--- src/components/index/IndexRandomGallery.jsx
+++ src/components/index/IndexRandomGallery.jsx
@@ -7,8 +7,11 @@ const IndexRandomGallery = ({ galleryItems }) => {
   return (
     <ul className="grid grid-cols-1 540:grid-cols-2 860:grid-cols-4 gap-24 px-32 w-full">
       {selected.map(({ frontmatter: { title, image } }) => (
-        <li className="p-24 bg-brown bg-opacity-5 rounded-4 space-y-16">
-          <div class="bg-white aspect-[16/9]">
+        <li
+          className="p-24 bg-brown bg-opacity-5 rounded-4 space-y-16"
+          key={title}
+        >
+          <div className="bg-white aspect-[16/9]">
             <img
               src={`/images/gallery/${image}`}
               alt={`Screenshot of ${title}`}
