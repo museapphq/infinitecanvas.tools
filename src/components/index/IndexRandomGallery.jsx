5645b620cd49cd06694c7539c2a418d4a85bde16
Arun Venkatesan <dallasarun@gmail.com>
2022-09-14 14:31:38 -0700

    finish history on index

--- src/components/index/IndexRandomGallery.jsx
+++ src/components/index/IndexRandomGallery.jsx
@@ -5,7 +5,7 @@ const IndexRandomGallery = ({ galleryItems }) => {
   const selected = shuffled.slice(0, NUMBER_OF_ITEMS_TO_SELECT)
 
   return (
-    <ul className="grid grid-cols-1 540:grid-cols-2 860:grid-cols-4 gap-x-24 640:gap-x-48 gap-y-48 w-full">
+    <ul className="grid w-full grid-cols-1 540:grid-cols-2 860:grid-cols-4 gap-x-24 640:gap-x-48 gap-y-48">
       {selected.map(({ frontmatter: { title, image } }) => (
         <li className="space-y-8" key={title}>
           <div className="aspect-[16/9]">
@@ -14,7 +14,7 @@ const IndexRandomGallery = ({ galleryItems }) => {
               alt={`Screenshot of ${title}`}
             />
           </div>
-          <h2 className="font-serif text-20 text-gray">{title}</h2>
+          <h2 className="font-serif text-16 text-gray">{title}</h2>
         </li>
       ))}
     </ul>
