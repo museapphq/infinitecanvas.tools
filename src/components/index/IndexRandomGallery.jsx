9618a2806db14636ad9b6b004b76af472932e446
Arun Venkatesan <dallasarun@gmail.com>
2022-09-21 12:56:21 -0700

    turn array of markdown files into map

--- src/components/index/IndexRandomGallery.jsx
+++ src/components/index/IndexRandomGallery.jsx
@@ -1,24 +1,37 @@
 const NUMBER_OF_ITEMS_TO_SELECT = 4
 
 const IndexRandomGallery = ({ galleryItems }) => {
-  const shuffled = galleryItems.sort(() => 0.5 - Math.random())
-  const selected = shuffled.slice(0, NUMBER_OF_ITEMS_TO_SELECT)
+  const filePathToName = (filePath) =>
+    filePath.match(/([^/]*).md$/g)[0].replace(".md", "")
+
+  const galleryMap = galleryItems.reduce((map, { file, frontmatter }) => {
+    map[filePathToName(file)] = { frontmatter }
+    return map
+  }, {})
+
+  console.log(galleryMap)
 
   return (
-    <ul className="grid w-full grid-cols-1 540:grid-cols-2 860:grid-cols-4 gap-x-24 640:gap-x-48 gap-y-48">
-      {selected.map(({ frontmatter: { title, image } }) => (
-        <li className="space-y-8" key={title}>
-          <div className="aspect-[16/9]">
-            <img
-              src={`/images/gallery/${image}`}
-              alt={`Screenshot of ${title}`}
-            />
-          </div>
-          <h2 className="font-serif text-16 text-gray">{title}</h2>
-        </li>
-      ))}
-    </ul>
+    <div>sdfsdf</div>
+    // <ul className="grid w-full grid-cols-1 540:grid-cols-2 860:grid-cols-4 gap-x-24 640:gap-x-48 gap-y-48">
+    //   {selected.map(({ frontmatter: { title, image } }) => (
+    //     <li className="space-y-8" key={title}>
+    //       <div className="aspect-[16/9]">
+    //         <img
+    //           src={`/images/gallery/${image}`}
+    //           alt={`Screenshot of ${title}`}
+    //         />
+    //       </div>
+    //       <h2 className="font-serif text-16 text-gray">{title}</h2>
+    //     </li>
+    //   ))}
+    // </ul>
   )
 }
 
 export default IndexRandomGallery
+
+// - Whiteboarding: Miro, FigJam,
+// - Design: Sketch, Figma, Xd
+// - Thinking: Muse, Concepts, MindNode, Heptabase, Kinopio
+// - Wildcard: Felt, Nodes, Origami, Bezel
